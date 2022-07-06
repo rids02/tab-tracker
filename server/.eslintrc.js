f5f194ead2534355a497e5b205198875e0508609
rids02 <21f1004802@student.onlinedegree.iitm.ac.in>
2022-07-06 15:44:18 +0530

    setup of the expressjs server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "amd": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
         "plugin:vue/vue3-essential"
     ],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        "ecmaVersion": "latest"
     },
     "plugins": [
         "vue"
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// console.log('hello');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json()) //to allow our app any json request that are sent in
+app.use(cors()) //to allow any host or client to access
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world! Riddhi here!'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
